01a8e02221159bc04020158b215dbbf939ef3c0d
Sibdroid <ryabovglebrg@gmail.com>
2024-10-08 01:21:41 +0200

    feat: use a smaller example, figure out the timeouts

--- main.py
+++ main.py
@@ -18,15 +18,18 @@ class AppURLopener(urllib.request.FancyURLopener):
 def request_price(s: pd.Series) -> str:
     code, color = s["code"], s["color"]
     link = f"https://www.bricklink.com/catalogPG.asp?P={code}&ColorID={color}"
+    print(link)
     opener = AppURLopener()
     response = opener.open(link)
     html = response.read()
     soup = BeautifulSoup(html, features="lxml")
     tables = soup.find_all("table", {"class": "fv"})
     new, used = map(lambda x: x.text, tables[1:3])
-    print(new)
-    print(used)
-    time.sleep(2)
+    with open("data.txt", "a") as file:
+        file.write(new+"\n")
+        file.write("\n")
+        print("done")
+    time.sleep(10)
 
 
 def parse_details(path: str) -> None:
@@ -37,11 +40,17 @@ def parse_details(path: str) -> None:
     for i, name in enumerate(columns):
         df[name] = [val[i] for val in df["main"]]
     df = df[columns]
+    with open("data.txt", "w") as file:
+        pass
     df.apply(request_price, axis=1)
 
 
+def file_to_human(df_path: str, file_path: str) -> None:
+    pass
+
 def main():
-    parse_details("falcon.csv")
+    #parse_details("example-tiny.csv")
+    pass
 
 
 if __name__ == "__main__":
